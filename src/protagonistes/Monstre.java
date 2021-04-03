1927f0ffa485c0fdf33c694532435cc3011236d2
Alexandre Coulais <alexandre64coulais@gmail.com>
2021-04-03 23:18:05 +0200

    Revert "Implémentation premières classes"
    This reverts commit 9b4549335130d59db0ede4dbf0462714e9769aae

--- src/protagonistes/Monstre.java
+++ src/protagonistes/Monstre.java
@@ -1,12 +1,5 @@
 package protagonistes;
 
-public class Monstre extends EtreVivant {
-    public Monstre(String tName) {
-        super(tName, 5);
-    }
-
-    public String attaquer(Personnage tPersonnage) {
-        String texte = "";
-        return texte;
-    }
+public class Monstre {
+    
 }
--- src/protagonistes/Personnage.java
+++ src/protagonistes/Personnage.java
@@ -1,45 +1,5 @@
 package protagonistes;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import environnement.Labyrinthe;
-import environnement.Tresor;
-import equipement.Arme;
-import equipement.Armure;
-import equipement.Potion;
-
-public class Personnage extends EtreVivant {
-    private int mBourse;
-    private Arme mArme;
-    private Armure mArmure;
-    private List<Potion> mPotions;
-    private Labyrinthe mLabyrinthe;
-
-    public Personnage(String tName) {
-        super(tName, 10);
-        this.mArme = null;
-        this.mArmure = null;
-        this.mPotions = new ArrayList<Potion>();
-        this.mLabyrinthe = null;
-    }
-
-    public String obtenirTresor(Tresor tTresor) {
-        String texte = "";
-
-        switch (tTresor.getType()) {
-            case ARME:
-                this.prendreArme(tTresor.getContenu());
-                break;
-            case ARMURE:
-                this.equiperArmure(tTresor.getContenu());
-                break;
-            case PIECE_OR:
-                this.mBourse += tTresor.getContenu();
-                break;
-            default:
-        }
-
-        return texte;
-    }
+public class Personnage {
+    
 }
