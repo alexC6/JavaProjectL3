4928a14f3b5d23003331b0de7949738192769a27
Perrine Mortier <perrine.mortier.fr@gmail.com>
2021-04-04 15:00:02 +0200

    Modification classe Porte - Ajout constructeur

--- src/environnement/Porte.java
+++ src/environnement/Porte.java
@@ -1,5 +1,11 @@
 package environnement;
 
+import controleur.Orientation;
+
 public class Porte {
-    
+
+    private Orientation mOrientationP;
+
+    public Porte ( Orientation tOrientationP){
+        this.mOrientationP = tOrientationP;    }
 }
