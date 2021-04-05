9f06c279e0b5d6d1b9a961234c7b4ae1d6f0a149
Alexandre <alexandre64coulais@gmail.com>
2021-04-05 11:11:40 +0200

    Implémentation basique de Combat

--- src/controleur/Combat.java
+++ src/controleur/Combat.java
@@ -1,5 +1,31 @@
 package controleur;
 
+import protagonistes.EtreVivant;
+import protagonistes.Monstre;
+import protagonistes.Personnage;
+
 public class Combat {
-    
+    private int mNbTours;
+    private Personnage mPersonnage;
+    private Monstre mMonstre;
+
+    public Combat(Personnage tPersonnage, Monstre tMonstre) {
+        this.mNbTours = 0;
+        this.mPersonnage = tPersonnage;
+        this.mMonstre = tMonstre;
+    }
+
+    public String rejointCombat(EtreVivant tEtreVivant) {
+        String texte = "";
+        return texte;
+    }
+
+    public EtreVivant vainqueur() {
+        return this.mPersonnage.isVivant() ? (EtreVivant) this.mPersonnage : (EtreVivant) this.mMonstre;
+    }
+
+    public String itererTour() {
+        String texte = "";
+        return texte;
+    }
 }
