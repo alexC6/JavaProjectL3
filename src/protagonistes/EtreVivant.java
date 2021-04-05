ffb9fed6c44f8d2d127742eba5a27575b36f017c
Alexandre <alexandre64coulais@gmail.com>
2021-04-05 10:35:30 +0200

    Implémentation constructeur EtreVivant

--- src/protagonistes/EtreVivant.java
+++ src/protagonistes/EtreVivant.java
@@ -1,5 +1,15 @@
 package protagonistes;
 
-public class EtreVivant {
-    
+import controleur.Combat;
+
+public abstract class EtreVivant {
+    private String mName;
+    private int mPointsDeVie;
+    private Combat mCombat;
+
+    protected EtreVivant(String tName, int tPointsDeVie) {
+        this.mName = tName;
+        this.mPointsDeVie = tPointsDeVie;
+        this.mCombat = null;
+    }
 }
