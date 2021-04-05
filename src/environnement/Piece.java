4a2014f346255b0e2b9fd702879f0eac42732700
Alexandre <alexandre64coulais@gmail.com>
2021-04-05 10:50:48 +0200

    Implémentation basique de Piece

--- src/environnement/Piece.java
+++ src/environnement/Piece.java
@@ -1,5 +1,38 @@
 package environnement;
 
+import java.util.List;
+
+import controleur.Combat;
+import controleur.Orientation;
+import protagonistes.Monstre;
+
 public class Piece {
-    
+    private int mNbPortes, mLigne, mColonne;
+    private List<Orientation> mPortes;
+    private Monstre mMonstre;
+    private Tresor mTresor;
+    private Combat mCombat;
+
+    private final int NB_MAX_PORTES, NB_MIN_PORTES;
+
+    public Piece() {}
+
+    public List<Orientation> getNbPortes() {
+        return this.mPortes;
+    }
+
+    public String genererMonstre() {
+        String texte = "";
+        return texte;
+    }
+
+    public String genererTresor() {
+        String texte = "";
+        return texte;
+    }
+
+    public String supprimerTresor() {
+        String texte = "";
+        return texte;
+    }
 }
