3d4824aa61ab710a4a25d328eaf7ffa6f1c39836
Alexandre <alexandre64coulais@gmail.com>
2021-04-05 10:46:11 +0200

    Implémentation basique de Personnage

--- src/protagonistes/Personnage.java
+++ src/protagonistes/Personnage.java
@@ -1,5 +1,33 @@
 package protagonistes;
 
-public class Personnage {
-    
+import environnement.Porte;
+import environnement.Tresor;
+import equipement.Potion;
+
+public class Personnage extends EtreVivant {
+    private String mName;
+
+    public Personnage(String tName) {
+        super(10);
+        this.mName = tName;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    String obtenirTresor(Tresor tTresor) {
+        String texte = "";
+        return texte;
+    }
+
+    String boirePotion(Potion tPotion) {
+        String texte = "";
+        return texte;
+    }
+
+    String ouvrirPorte(Porte tPorte) {
+        String texte = "";
+        return texte;
+    }
 }
