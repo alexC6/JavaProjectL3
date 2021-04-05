aed78ae649cbae151c263d5f02ef7f71eca13d4e
Alexandre <alexandre64coulais@gmail.com>
2021-04-05 10:38:44 +0200

    Implémentation basique des méthodes EtreVivant

--- src/protagonistes/EtreVivant.java
+++ src/protagonistes/EtreVivant.java
@@ -12,4 +12,41 @@ public abstract class EtreVivant {
         this.mPointsDeVie = tPointsDeVie;
         this.mCombat = null;
     }
+
+    public int getPointDeVie() {
+        return this.mPointsDeVie;
+    }
+
+    public String getName() {
+        return this.mName;
+    }
+
+    public boolean isVivant() {
+        return this.mPointsDeVie > 0;
+    }
+
+    public String mourir() {
+        String texte = "";
+        return texte;
+    }
+
+    public String subirAttaque(int tDegats) {
+        String texte = "";
+
+        if (tDegats >= 0) {
+            this.mPointsDeVie -= tDegats;
+        }
+
+        return texte;
+    }
+
+    public String recupererVie(int tPointsDeVie) {
+        String texte = "";
+
+        if (tPointsDeVie > 0) {
+            this.mPointsDeVie = tPointsDeVie;
+        }
+
+        return texte;
+    }
 }
