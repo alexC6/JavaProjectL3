f21d6e7fc0c26995b8cb92cfb9ba685ca522a668
Alexandre <alexandre64coulais@gmail.com>
2021-04-09 11:47:40 +0200

    Réimplémentation de mourir() et attaquer()

--- src/protagonistes/Monstre.java
+++ src/protagonistes/Monstre.java
@@ -12,16 +12,26 @@ public class Monstre extends EtreVivant {
         super(5);
     }
 
-    public String attaquer(Personnage tPersonnage) {
+    public String attaquer() {
         String texte = "";
+        Personnage victime = this.mCombat.getPersonnage();
 
-        texte += "Vous êtes attaqué par le monstre !\n";
-        texte += tPersonnage.subirAttaque(1);
+        if (victime != null) {
+            texte += "Vous êtes attaqué par le monstre !\n";
+            texte += victime.subirAttaque(1);
+        } else {
+            texte += "Oups ! Vous êtes mort, le monstre ne peut pas vous attaquer.\n";
+        }
 
         return texte;
     }
 
     public String mourir() {
-        return "Bravo, vous avez terrassé le terrible monstre !\n";
+        String texte = "";
+        
+        texte += this.quitteCombat();
+        texte += "Bravo, vous avez terrassé le terrible monstre !\n";
+
+        return texte;
     }
 }
