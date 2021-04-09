4c27cc095e0417e772ba388b755a1050699eb96e
Alexandre <alexandre64coulais@gmail.com>
2021-04-09 16:00:56 +0200

    Implémentation constructeur et obtenirTresor()

--- src/protagonistes/Personnage.java
+++ src/protagonistes/Personnage.java
@@ -7,21 +7,38 @@
  */
 package protagonistes;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import environnement.Labyrinthe;
 import environnement.Porte;
 import environnement.Tresor;
+import environnement.TypeTresor;
+import equipement.Arme;
+import equipement.Armure;
 import equipement.Potion;
 
 public class Personnage extends EtreVivant {
+    private int mBourse;
     private String mName;
+    private Arme mArme;
+    private Armure mArmure;
+    private List<Potion> mPotions = new ArrayList<Potion>();
+    private Labyrinthe mLabyrinthe;
 
     public Personnage(String tName) {
         super(10);
+        this.mBourse = 0;
         this.mName = tName;
+        this.mArme = null;
+        this.mArmure = null;
+        this.mLabyrinthe = null;
     }
 
     public String attaquer(){
         //mcombat de etre vivant +combat methode get monstre 
     
+        // TODO
     }
     public String getName() {
         return this.mName;
@@ -29,6 +46,34 @@ public class Personnage extends EtreVivant {
 
     String obtenirTresor(Tresor tTresor) {
         String texte = "";
+        TypeTresor type = tTresor.getType();
+
+        switch (type) {
+            case ARME:
+                texte += this.prendreArme((Arme) tTresor.getContenu());
+                break;
+            case ARMURE:
+                texte += this.equiperArmure((Armure) tTresor.getContenu());
+                break;
+            case PIECE_OR:
+                this.mBourse += (int) tTresor.getContenu();
+                break;
+        }
+        
+        tTresor.viderTresor();
+
+        return texte;
+    }
+
+    public String prendreArme(Arme tArme) {
+        String texte = "";
+        // TODO
+        return texte;
+    }
+
+    public String equiperArmure(Armure tArmure) {
+        String texte = "";
+        // TODO
         return texte;
     }
 
@@ -43,6 +88,7 @@ public class Personnage extends EtreVivant {
 
     String ouvrirPorte(Porte tPorte) {
         String texte = "";
+        // TODO
         return texte;
     }
 
