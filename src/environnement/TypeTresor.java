b0a16b8941a5a1ffc5d22eb62e4640c1907a521c
Alexandre <alexandre64coulais@gmail.com>
2021-04-10 11:24:19 +0200

    Enrichissement avec noms

--- src/environnement/TypeTresor.java
+++ src/environnement/TypeTresor.java
@@ -1,5 +1,17 @@
 package environnement;
 
 public enum TypeTresor {
-    ARME,ARMURE,PIECE_OR;
+    ARME("une arme"),
+    ARMURE("une armure"),
+    PIECE_OR("des pièces d'or");
+
+    private String mNom;
+
+    TypeTresor(String tNom) {
+        this.mNom = tNom;
+    }
+
+    public String toString() {
+        return this.mNom;
+    }
 }
