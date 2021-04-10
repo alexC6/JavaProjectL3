fa8416b1932aa1a247401941608e2481cf0a7e0e
Alexandre <alexandre64coulais@gmail.com>
2021-04-10 11:20:52 +0200

    Version actuelle de Piece finie

--- src/environnement/Piece.java
+++ src/environnement/Piece.java
@@ -1,21 +1,49 @@
 package environnement;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import controleur.Combat;
 import controleur.Orientation;
+import equipement.Arme;
+import equipement.Armure;
 import protagonistes.Monstre;
 
 public class Piece {
     private int mNbPortes, mLigne, mColonne;
-    private List<Orientation> mPortes;
+    private List<Orientation> mPortes = new ArrayList<Orientation>();
+    private Labyrinthe mLabyrinthe;
     private Monstre mMonstre;
     private Tresor mTresor;
     private Combat mCombat;
 
-    private final int NB_MAX_PORTES, NB_MIN_PORTES;
+    private final int NB_MAX_PORTES = 1, NB_MIN_PORTES = 4;
 
-    public Piece() {}
+    public Piece(List<Orientation> tPortes, int tLigne, int tColonne, Labyrinthe tLabyrinthe) {
+        Random rand = new Random();
+        int tresorType = rand.nextInt(3);
+        
+        this.mNbPortes = tPortes.size();
+        this.mLigne = tLigne;
+        this.mColonne = tColonne;
+        this.mPortes = tPortes;
+        this.mLabyrinthe = tLabyrinthe;
+        this.mMonstre = new Monstre();
+        this.mCombat = new Combat(mLabyrinthe.getPersonnage(), this.mMonstre);
+
+        switch (tresorType) {
+            case 0:
+                this.mTresor = new Tresor<Arme>(new Arme(), TypeTresor.ARME, this);
+                break;
+            case 1:
+                this.mTresor = new Tresor<Armure>(new Armure(), TypeTresor.ARMURE, this);
+                break;
+            case 2:
+                int piecesOr = rand.nextInt(100) + 1;
+                this.mTresor = new Tresor<Integer>(piecesOr, TypeTresor.PIECE_OR, this);
+        }
+    }
 
     public int getNbPortes() {
         return this.mNbPortes;
@@ -25,18 +53,7 @@ public class Piece {
         return this.mPortes;
     }
 
-    public String genererMonstre() {
-        String texte = "";
-        return texte;
-    }
-
-    public String genererTresor() {
-        String texte = "";
-        return texte;
-    }
-
-    public String supprimerTresor() {
-        String texte = "";
-        return texte;
+    public void supprimerTresor() {
+        this.mTresor = null;
     }
 }
