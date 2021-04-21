0aab9e3c6be8d1ead33690cab37a536ac8d6d6f5
Perrine Mortier <perrine.mortier.fr@gmail.com>
2021-04-21 13:42:17 +0200

    Codage enum Orientation : Ajout méthode toString

--- src/controleur/Orientation.java
+++ src/controleur/Orientation.java
@@ -2,9 +2,22 @@ package controleur;
 
 public enum Orientation {
 
-    NORD,
-    SUD,
-    EST, 
-    OUEST;
+    NORD ("NORD"),
+    SUD ("SUD"),
+    EST("EST"),
+    OUEST("OUEST");
+
+    private String nomOrientation;
+
+    Orientation(String nomOrientation){
+
+        this.nomOrientation = nomOrientation;
+    }
+
+    public String toString(){
+        
+        return nomOrientation;
+    }
+
     
-}
+}// FIN CLASS ENUM ORIENTATION
