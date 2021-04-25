6b36f17aba8390131efa8718bc04268c2435c14e
Perrine Mortier <perrine.mortier.fr@gmail.com>
2021-04-25 17:15:44 +0200

    Codage corps méthode générerPieces

--- src/environnement/Labyrinthe.java
+++ src/environnement/Labyrinthe.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import controleur.Orientation;
 import environnement.*;
 
 public class Labyrinthe {
@@ -19,8 +20,8 @@ public class Labyrinthe {
     private Piece mEntree;
     private Personnage mPersonnage;
 
-    private static final NB_LIGNES_MIN  = 3;
-    private static final NB_LIGNES_MAX = 6 ;
+    private static final int NB_LIGNES_MIN  = 3;
+    private static final int NB_LIGNES_MAX = 6 ;
 
     public Labyrinthe() {
 
@@ -54,28 +55,100 @@ public class Labyrinthe {
     // C'est au labyrinthe de faire appel a la crea piece en fonction de la position du perso a ce moment là
     public String changerDePiece(){
         String texte ="";
+
         return texte;
+
+
     }
     // Pas de traitement de cette méthode pr le moment 
     public String chargerLabyrinthe(int tLigne, int tCol){
+
         String texte ="";
+
         texte += "Le labyrinthe est en cours de création. Veuillez patienter, noble combattant ! \n";
+
         // VOIR AVEC ALEX COMMENT INTEGRER UN DELAI D ATTENTE
         this.mNbPieces = tLigne * tCol;
-        texte += "Vous allez voyager au sein d'un labyrinthe composé de " + this.getNbPieces() + ".\n";       
+
+        texte += "Vous allez voyager au sein d'un labyrinthe composé de " + this.getNbPieces() + ".\n";    
+
         return texte;
+
     }
 
-    // Trad : faire appel à new piece ds un emplacement du tableau
-    // Generer une liste de portes que la piece va contenir 
-    // Attention : aux bords du labyrinthe 
-    String genererPiece(){
+    
+    public String genererPieces(){
+
+        // Indices de parcours de la matrice carrée : i pour ligne & j pour colonne 
+        int i,j;
+        int sizeOfTab = this.mMatricePieces.length;
+        // Initialisation de la liste des portes disponibles
+        List<Porte> portes = new ArrayList <Porte>();
+
+        // Création des portes pour chaque cardinalité
+        Orientation orientationPN = Orientation.NORD;
+        Porte porteN = new Porte(orientationPN);
+
+        Orientation orientationPS = Orientation.SUD;
+        Porte porteS = new Porte(orientationPS);
+
+        Orientation orientationPE = Orientation.EST;
+        Porte porteE = new Porte(orientationPE);
+
+        Orientation orientationPO = Orientation.OUEST;
+        Porte porteO = new Porte(orientationPO);
+
+        // List which contains all the doors 
+        portes.add(porteN);
+        portes.add(porteS);
+        portes.add(porteE);
+        portes.add(porteO);
         
         String texte ="";
-        
+
+        for ( i = 0; i<sizeOfTab; i++){
+
+            for ( j = 0; j< sizeOfTab; j++){
+
+
+                // Remove the door according to the coord of the room
+                
+                if ( i == 0) {
+                    // If first line of Matrix so not North Door
+                    portes.remove(porteN);
+                }
+
+                // If last line of Matrix so not South Door
+                else  if ( i == (sizeOfTab-1)) {
+                    portes.remove(porteS);
+                }
+
+                // If first column of Matrix so not West Door
+                if ( j == 0 ){
+                    portes.remove(porteO);
+                }
+
+                // If last Column of Matrix so not East Door 
+                else if ( j == (sizeOfTab - 1)){
+                    portes.remove(porteE);
+
+                }
+
+                // Add to the Lab the room that contains compatible doors
+                this.mMatricePieces[i][j] = new Piece(portes,i,j,this);
+            }// End of pathway column
+            
+        } // End of pathway line
+
         return texte;
     }
 
+    
+    /**
+     * 
+     * @param tPiece
+     * @return
+     */
     public boolean isEntree(Piece tPiece){
         return( tPiece == this.mEntree);
     }
@@ -84,14 +157,31 @@ public class Labyrinthe {
        return ( tPiece == this.mSortie);
     }
 
+    /**
+     * Getter du nombre de pièces
+     * 
+     * @return int  Nombre de pièces du labyrinthe
+     */
+
     public int getNbPieces(){
         return this.mNbPieces;
     }
+
+    /**
+     * Getter du nombre de boutiques
+     * 
+     * @return int  Nombre de boutiques du labyrinthe 
+     */
 
     public int getNbBoutiques(){
         return this.mNbBoutiques;
     }
 
+    /**
+     * Getter du personnage
+     * 
+     * @return Personnage personnage relié au Labyrinthe
+     */
     public Personnage getPersonnage(){
         return this.mPersonnage;
     }
