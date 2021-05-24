0f214f9a42dcc65d286057c625dfaef264237969
Alexandre Coulais <alexandre64coulais@gmail.com>
2021-05-24 11:40:52 +0200

    Reformatage de Boutique

--- src/equipement/Potion.java
+++ src/equipement/Potion.java
@@ -3,45 +3,45 @@ package equipement;
 import java.io.Serializable;
 
 import protagonistes.Personnage;
-public class Potion implements Serializable { 
+
+public class Potion implements Serializable {
 
     private Personnage mProprietaire;
     private int mRecuperation;
-    private int FORCE_POTION_MIN =1;
-    private int FORCE_POTION_MAX =10;
+    private int FORCE_POTION_MIN = 1;
+    private int FORCE_POTION_MAX = 10;
 
-    public Potion(final int ... args) {
+    public Potion(final int... args) {
         this.mProprietaire = null;
-        if(args.length>0){
-            mRecuperation=args[0];
-        }else{
-        //calcul du nombre de point de vie récuperés (entre 1 et 10)
-        this.mRecuperation =(int) (Math.random() * ( FORCE_POTION_MAX - FORCE_POTION_MIN ));
+        if (args.length > 0) {
+            mRecuperation = args[0];
+        } else {
+            // calcul du nombre de point de vie récuperés (entre 1 et 10)
+            this.mRecuperation = (int) (Math.random() * (FORCE_POTION_MAX - FORCE_POTION_MIN));
         }
     }
 
-    public Potion(Personnage tProprietaire, final int ... args) {
+    public Potion(Personnage tProprietaire, final int... args) {
         this.mProprietaire = tProprietaire;
-        //calcul du nombre de point de vie récuperés (entre 1 et 10)
-       
-        if(args.length>0){
-            mRecuperation=args[0];
-         }else{
-            this.mRecuperation =(int) (Math.random() * ( FORCE_POTION_MAX - FORCE_POTION_MIN ));
-         }
-    }
-    
-    public void viderPotion(){
-        this.mProprietaire = null;
+        // calcul du nombre de point de vie récuperés (entre 1 et 10)
+
+        if (args.length > 0) {
+            mRecuperation = args[0];
+        } else {
+            this.mRecuperation = (int) (Math.random() * (FORCE_POTION_MAX - FORCE_POTION_MIN));
+        }
     }
 
+    public void viderPotion() {
+        this.mProprietaire = null;
+    }
 
     public int getRecuperation() {
         return mRecuperation;
     }
 
-    public String afficherRecuperation(){
-        return "Vous avez récupéré "+this.mRecuperation+" point(s) de vie.\n";
+    public String afficherRecuperation() {
+        return "Vous avez récupéré " + this.mRecuperation + " point(s) de vie.\n";
     }
 
     public String toString() {
