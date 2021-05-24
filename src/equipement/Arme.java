3b1a1b7b7ae1d19aec83fd404361afb33ba419df
Alexandre Coulais <alexandre64coulais@gmail.com>
2021-05-24 11:40:07 +0200

    Commentaires des classes Arme et Armure

--- src/equipement/Arme.java
+++ src/equipement/Arme.java
@@ -16,23 +16,33 @@ public class Arme implements Serializable {
     private int DOMMAGES_MAX =5;
     private Personnage mProprietaire;
 
-    public Arme(final int ... args) {
-        this.mProprietaire = null; 
-        //calcul du nombre de point de dommage (entre 1 et 5)
-        if(args.length>0){
-            this.mPointDommage=args[0];
-        }else{
-        this.mPointDommage = (int) (Math.random() * ( DOMMAGES_MAX - DOMMAGES_MIN ));
+
+    /**
+     *
+     * @param args (permet de donnée l'argument que l'ont veux sinon aléatoire)
+     */
+    public Arme(final int... args) {
+        this.mProprietaire = null;
+        // calcul du nombre de point de dommage (entre 1 et 5)
+        if (args.length > 0) {
+            this.mPointDommage = args[0];
+        } else {
+            this.mPointDommage = (int) (Math.random() * (DOMMAGES_MAX - DOMMAGES_MIN));
         }
     }
 
-    public Arme(Personnage tProprietaire, final int ... args) {
-        this.mProprietaire = tProprietaire; 
+    /**
+     *
+     * @param tProprietaire
+     * @param args
+     */
+    public Arme(Personnage tProprietaire, final int... args) {
+        this.mProprietaire = tProprietaire;
         this.mProprietaire.prendreArme(this);
-        if(args.length>0){
-           this.mPointDommage=args[0];
-        }else{
-            this.mPointDommage = (int) (Math.random() * ( DOMMAGES_MAX - DOMMAGES_MIN ));
+        if (args.length > 0) {
+            this.mPointDommage = args[0];
+        } else {
+            this.mPointDommage = (int) (Math.random() * (DOMMAGES_MAX - DOMMAGES_MIN));
         }
     }
     
@@ -40,24 +50,43 @@ public class Arme implements Serializable {
         return mProprietaire;
     }
 
+    /**
+    *
+    *
+    */
     public void setProprietaire(Personnage tProprietaire) {
         this.mProprietaire = tProprietaire;
 
     }
 
+    /**
+     *
+     *Fonction permettant de lâcher l'arme porter par le joueur 
+     */
     public void lacher () {
         this.mProprietaire = null;
     }
 
-    public String attaquer(Monstre tMonstre)
-    { 
+    /**
+     * Fonction d'attaque du personnage avec une épée
+     *
+     * @param tMonstre le monstre de la salle
+     * @return Les dégats subit par le monstre apres l'attaque
+     */
+    public String attaquer(Monstre tMonstre) {
         return tMonstre.subirAttaque(this.mPointDommage);
     }
 
-    public int getDommageArme(){
+    
+    public int getDommageArme() {
         return this.mPointDommage;
     }
 
+    /**
+     * Fonction d'affichage des dommage causé par l'arme
+     *
+     * @return renvoie du texte avec les point de dommage de l'arme
+     */
     public String toString() {
         return "arme de dommage " + this.mPointDommage;
     }
--- src/equipement/Armure.java
+++ src/equipement/Armure.java
@@ -1,44 +1,59 @@
 package equipement;
+
 import java.io.Serializable;
 
 import protagonistes.Personnage;
+
+/**
+ * 
+ * @author Noëmie Suere
+ * @version 2021-5-24
+ */
 public class Armure implements Serializable {
     private int mProtection;
-    private int PROTECT_MIN=1;
-    private int PROTECT_MAX=5;
+    private int PROTECT_MIN = 1;
+    private int PROTECT_MAX = 5;
     private Personnage mProprietaire;
     private int mEtatInitial;
 
-   
-    public Armure(final int ... args) {
-        if(args.length>0){
-            mProtection =args[0];
+    /**
+     *
+     * @param args (permet de donnée l'argument que l'ont veux sinon aléatoire)
+     */
+    public Armure(final int... args) {
+        if (args.length > 0) {
+            mProtection = args[0];
             this.mEtatInitial = mProtection;
-         }else{
-           //calcul du nombre de point d'armure (entre 1 et 5)
-          this.mProtection =(int) (Math.random() * ( PROTECT_MAX - PROTECT_MIN ));
-          this.mEtatInitial = this.mProtection;
-         }
+        } else {
+            // calcul du nombre de point d'armure (entre 1 et 5)
+            this.mProtection = (int) (Math.random() * (PROTECT_MAX - PROTECT_MIN));
+            this.mEtatInitial = this.mProtection;
+        }
     }
 
-    public Armure(Personnage tProprietaire, final int ... args) {
+    /**
+     *
+     * @param tProprietaire
+     * @param args
+     */
+    public Armure(Personnage tProprietaire, final int... args) {
         this.mProprietaire = tProprietaire;
         this.mProprietaire.equiperArmure(this);
-        if(args.length>0){
-           mProtection =args[0];
-           this.mEtatInitial = mProtection;
-        }else{
-            this.mProtection =(int) (Math.random() * ( PROTECT_MAX - PROTECT_MIN ));
+        if (args.length > 0) {
+            mProtection = args[0];
+            this.mEtatInitial = mProtection;
+        } else {
+            this.mProtection = (int) (Math.random() * (PROTECT_MAX - PROTECT_MIN));
             this.mEtatInitial = this.mProtection;
 
         }
     }
-     
-    public int getEtatInitial(){
+
+    public int getEtatInitial() {
         return this.mEtatInitial;
     }
 
-    public int getPointsProtection(){
+    public int getPointsProtection() {
         return this.mProtection;
     }
 
@@ -46,41 +61,45 @@ public class Armure implements Serializable {
         return mProprietaire;
     }
 
+    /**
+    *
+    *
+    */
     public void setProprietaire(Personnage tProprietaire) {
-        if(this.mProprietaire==null){
+        if (this.mProprietaire == null) {
             this.mProprietaire = tProprietaire;
         }
     }
-   
-    public String encaisserDegat(int tDegat){
 
-        String texte1="Votre armure est cassé";
-        String texte2="Votre armure a subit"+tDegat;
+    public String encaisserDegat(int tDegat) {
 
-       if(tDegat<this.mProtection){
-        this.mProtection= this.mProtection-tDegat;
-        return texte2;
-       }else if(tDegat>this.mProtection){
+        String texte1 = "Votre armure est cassé";
+        String texte2 = "Votre armure a subit" + tDegat;
 
-        this.casser();
-        return texte1;
-       }
-       return null;
+        if (tDegat < this.mProtection) {
+            this.mProtection = this.mProtection - tDegat;
+            return texte2;
+        } else if (tDegat > this.mProtection) {
+
+            this.casser();
+            return texte1;
+        }
+        return null;
     }
 
-    public void casser(){
+    public void casser() {
         this.mProtection = 0;
     }
 
-    public String reparer(){
-        String texte="";
+    public String reparer() {
+        String texte = "";
 
-        if(this.mProtection< this.mEtatInitial){
+        if (this.mProtection < this.mEtatInitial) {
             this.mProtection = this.mEtatInitial;
-            texte=" Votre armure est réparée.";
-        }else if (this.mProtection == this.mEtatInitial ) {
-            texte="Vous n'avez besoin de réparer votre armure";
-        }       
+            texte = " Votre armure est réparée.";
+        } else if (this.mProtection == this.mEtatInitial) {
+            texte = "Vous n'avez besoin de réparer votre armure";
+        }
         return texte;
     }
 
