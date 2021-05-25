6399a5f7bb253d5755e969bfcf1ffef139fdea6b
Alexandre Coulais <alexandre64coulais@gmail.com>
2021-05-25 10:35:14 +0200

    Enrichissement de le classe principale

--- src/main/Jeu.java
+++ src/main/Jeu.java
@@ -10,27 +10,70 @@ import vue.BoundaryCombat;
 import vue.BoundaryDeplacer;
 import vue.BoundaryLancement;
 
+/**
+ * <p>
+ * File : Jeu.java
+ * Code source de la classe principale du jeu
+ * </p>
+ * 
+ * @author Alexandre Coulais, Noëmie Suere, Perrine Mortier, thomas Chabert
+ * 
+ * @version 2021-5-21
+ */
+
 public class Jeu {
     public static void main(String args[]) {
         boolean quitter = false;
+
+        // Récupération des objets nécessaires à l'utilisation des boundaries
         Labyrinthe labyrinthe = BoundaryLancement.lancerPartie();
         Personnage personnage = labyrinthe.getPersonnage();
 
+        // Création des boundaries de déplacement
         ControleurDeplacer ctrlDeplacement = new ControleurDeplacer(labyrinthe);
         BoundaryDeplacer bdyDeplacement = new BoundaryDeplacer(ctrlDeplacement);
         BoundaryBoutique.setEntree((Entree) labyrinthe.getPiece(0, 0));
-        
+
         while (!(quitter)) {
+            // Tant que le joueur ne veut pas quitter le jeu
+            // Appel du déplacement du personnage
             bdyDeplacement.deplacer();
-            
+
             if (!labyrinthe.isEntree(personnage.getPiece())) {
+                // Dans le cas où la salle est une pièce de combat
+                // On initialise les coundary et controleur de combat
                 ControleurCombat ctrlCombat = new ControleurCombat(personnage);
-                BoundaryCombat bdycombat = new BoundaryCombat(ctrlCombat);
-                // Appels boundary combat
-            } else {
+                BoundaryCombat bdyCombat = new BoundaryCombat(ctrlCombat);
+
+                if (bdyCombat.checkVainc() == false) {
+                    // S'il n'y a pas de vainqueur, on lance le combat
+                    bdyCombat.scenar();
+
+                    if (bdyCombat.choixCombatreFuir()) {
+                        // Si le joueur choisit de combattre, on lance le combat
+                        bdyCombat.demarrerCombat();
+
+                        // If de la potion, doit être gérée par les combats
+
+                        bdyCombat.lancerTour();
+
+                        while (bdyCombat.checkVainc() == false && bdyCombat.choixCombatreFuir()) {
+                            // Tant qu'il n'y a pas de mort et que le joueur veut continuer
+                            // On continue d'effectuer des tours de combat
+                            bdyCombat.lancerTour();
+                        }
+                    }
+                } else {
+                    // Si le monstre est mort, on indique au joueur que la pièce est vide
+                    bdyCombat.pieceVide();
+                }
+            } else if (bdyDeplacement.faireBoutique()) {
+                // Si le joueur souhaite faire les boutiques
                 BoundaryBoutique.entrerCentreCommercial();
             }
 
+            // On demande au personnage s'il veut sauvegarder sa partie
+            // Et dans ce cas, s'il souhaite quitter la partie
             quitter = BoundaryLancement.sauvegarderPartie(labyrinthe);
         }
     }
